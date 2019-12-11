14aa11124a3fa8df4de8672a95f952010d474700
LiaoChinTe <liao.chinte@gmail.com>
2019-12-11 18:04:40 +0100

    update quantum cheque, debugging

--- QuantumCheque/QuantumCheque.py
+++ QuantumCheque/QuantumCheque.py
@@ -1,5 +1,6 @@
 
 
+
 import numpy as np
 import netsquid as ns
 from netsquid.nodes.node import Node
@@ -8,28 +9,46 @@ from netsquid.qubits import ketstates
 from netsquid.qubits.operators import *
 from netsquid.qubits.qubitapi import *
 from netsquid.qubits import operators as ops
+from netsquid.components import QuantumMemory, DepolarNoiseModel 
+from netsquid.pydynaa import Entity,EventHandler,EventType
 
-from random import seed, randint
-
-
-
-
-
-def Create_GHZ_qubits(num_qubits=8):
-    qList = create_qubits(num_qubits,system_name="Q") #qubit 00
-    ns.qubits.operate(qList[0], ns.H)
-    ns.qubits.operate([qList[0],qList[1]], ns.CNOT) 
-    for i in range(2,num_qubits):
-        ns.qubits.operate([qList[0],qList[i]], ns.CNOT)
-    return qList
+from random import seed, randint,random
 
 
 
-def RandomSerialNumGen(num_bits):
-    seed(randint(0, 2**num_bits))
+
+def Create_GHZ_triple_qubits_list(num_qubits=8):
+    qListB=[]
+    qListA1=[]
+    qListA2=[]
+    for i in range(num_qubits):
+        qList = create_qubits(3) #qubit 000
+
+        
+        ns.qubits.operate(qList[0], ns.H)
+        ns.qubits.operate([qList[0],qList[1]], ns.CNOT) 
+        ns.qubits.operate(qList[1], ns.H)
+        ns.qubits.operate([qList[1],qList[2]], ns.CNOT)
+        
+        qListB.append(qList[0])
+        qListA1.append(qList[1])
+        qListA2.append(qList[2])
+        #print(qListB[0].qstate.dm)
+
+    return qListB,qListA1,qListA2
+
+
+
+
+
+
+def UniqueSerialNumGen(num_bits):
+    seed() 
     bitList=[]
+    startUSN=randint(0,1000)
     for i in range(num_bits):
-        bitList.append(randint(0,1))
+        bitList.append(startUSN)
+        startUSN+=1
     return bitList
 
 
@@ -63,6 +82,9 @@ def OneWayFunction(identity=None,symkey=[],randomSerialNumber=0,Money=0):
     #print(tempQubit.qstate.dm)
     return tempQubit
     
+    
+    
+    
 # C swap can be composed by T
 # https://www.mathstat.dal.ca/~selinger/quipper/doc/QuipperLib-GateDecompositions.html
 def Cswap(qA,qB,qC):
@@ -87,7 +109,6 @@ def Cswap(qA,qB,qC):
     operate([qB, qA], ops.CNOT)
     H | qC
     operate([qC, qB], ops.CNOT)
-    
     return qA,qB,qC
 
 
@@ -95,13 +116,38 @@ def Cswap(qA,qB,qC):
 def SwapTest(qB,qC):
     qA=create_qubits(1)
     qA=qA[0]
-
     H | qA
     Cswap(qA,qB,qC)
     H | qA
-
     return ns.qubits.qubitapi.measure(qA,observable=Z)
     
+    
+    
+def Fix_measure(tmp): #deal with dm like [0,0.4]
+    if tmp[1] <= random():
+        return 1-tmp[0]
+    else:
+        return tmp[0]
+    
+
+def Fix_measure_simple(tmp): #deal with dm like [0,0.4]
+    if  tmp[1] == 0.5 :
+        return tmp[0]
+    elif any( [tmp[0] == 1 and tmp[1] > 0.5
+        , tmp[0] == 0 and tmp[1] < 0.5] ):
+        return 1
+    elif any( [tmp[0] == 0 and tmp[1] > 0.5
+        , tmp[0] == 1 and tmp[1] < 0.5] ):
+        return 0
+    
+    else:
+        print(tmp)
+        print("measurement ERROR!")
+        
+        return -1
+
+
+
 
 
 
@@ -109,81 +155,181 @@ def SwapTest(qB,qC):
 
 class QuantumCheque(Protocol):
   
+
     # QuantumCheque function =========================================================
 
-    def B_send_checkbook(self):
-        print("B_send_checkbook called")
-        self.BChequeBook=Create_GHZ_qubits(self.num_bits)
-        # save one qubit for bank
-        self.saveChequeBook=self.BChequeBook[0]
-        # send others to A
-        self.node_B.ports["portQ_B1"].tx_output(self.BChequeBook[1:])
-    
-    
-    
-    def A_RSG_send_B(self,qList):
-        print("A_RSG_send_B called")
-        self.AChequeBook=qList.items
-        self.A_saveRSB=RandomSerialNumGen(self.num_bits)
-        self.node_A.ports["portC_A1"].tx_output(self.A_saveRSB)
-    
-   
-
-    def BA_BB84_keygen(self,RSB,num_bits=8,fibre_len=10**-6,
+    def BA_BB84_keygen(self,num_bits=8,fibre_len=10**-6,
                  fibre_loss_init=0,fibre_loss_length=0):
         print("BA_BB84_keygen")
-        self.B_saveRSB=RSB
-        loc_BB84=BB84(num_bits=num_bits,fibre_len=fibre_len)
+        loc_BB84=BB84(num_bits=num_bits,fibre_len=fibre_len,
+            fibre_loss_init=fibre_loss_init,fibre_loss_length=fibre_loss_init)
+        # assign keys to A and B
         self.key_BB84_A=loc_BB84.key_A
         self.key_BB84_B=loc_BB84.key_B
         # trigger A's one way function
-        self.node_B.ports["portC_B2"].tx_output('Bob')
+        self.node_A.ports["portC_A1"].tx_output('Alice')
         
         
         
-    def A_owf_send_C(self,identity):
-        identity=identity.items[0]
-        
-        if str(identity)=='Bob':
-            res_owf_qubit=OneWayFunction(identity,self.key_BB84_A,self.A_saveRSB.pop(),456)
-
-        
-            if len(self.AChequeBook)<=0:
-                print("No more cheque! Aborting!")
-                return 0
-            else:
-                # Alice performs Bell state measurement
-                operate([res_owf_qubit, self.AChequeBook.pop()], ops.CNOT)
-                H | res_owf_qubit
-            
-                # not done
-                #
-                #
-            
-            
-            
-            self.node_A.ports["portQ_A2"].tx_output(res_owf_qubit)
+    def B_send_GHZ(self,username):
+        print("B_send_GHZ called")
+        if username.items[0]=='Alice':
+            self.BChequeBook,tmpAChequeBook,tmpA2ChequeBook=Create_GHZ_triple_qubits_list(self.num_bits)
+            tmpAChequeBook.extend(tmpA2ChequeBook)
+            self.node_B.ports["portQ_B1"].tx_output(tmpAChequeBook)
         else:
-            print("Error! message not received from Bob!")
+            print("User name ERROR!")
+    
+    
+    
+    def A_req_USN(self,tmpAChequeBook):
+        self.A1ChequeBook=tmpAChequeBook.items[:self.num_bits]
+        self.A2ChequeBook=tmpAChequeBook.items[self.num_bits:]
+        self.node_A.ports["portC_A2"].tx_output("USN_req")
+        
+        
+        
+    
+    def B_USN_send_A(self,req): 
+        print("B_USN_send_A called")
+        self.B_saveUSN=UniqueSerialNumGen(self.num_bits)
+        #print(self.B_saveUSN)
+        self.node_B.ports["portC_B3"].tx_output(self.B_saveUSN)
+        
+        
+        
+        
+    def A_owf_send_C(self,saveUSN):
+        print("A_owf_send_C called")
+        self.A_saveUSN=saveUSN.items
+        #print("self.A_saveUSN=",self.A_saveUSN)
+        if len(self.A1ChequeBook) < self.num_bits:
+            print("No more cheque! Aborting!")
+            return 0
+        else:
             
 
-    # a list of qubits as Qcheque is ready to be verified by bank
-    def CB_Verify(self,chequeQ):
+            for i in range(self.num_bits):
+                #print(self.Money)
+                
+                # Alice write down the amound of money 
+                res_owf_qubit=OneWayFunction('Alice',self.key_BB84_A
+                    ,self.A_saveUSN[i],self.Money)
+                
+                # Alice performs Bell state measurement
+                operate([res_owf_qubit, self.A1ChequeBook[i]], ops.CNOT)     
+                H | res_owf_qubit
+
+                mes_A1=ns.qubits.qubitapi.measure(self.A1ChequeBook[i]
+                    ,observable=Z)
+
+                mes_owf_qubit=ns.qubits.qubitapi.measure(res_owf_qubit
+                    ,observable=Z)
+
+                if Fix_measure(mes_A1) == 0   and Fix_measure(mes_owf_qubit) == 1:
+                    Z | self.A2ChequeBook[i]
+                if Fix_measure(mes_A1) == 1   and Fix_measure(mes_owf_qubit) == 0:
+                    X | self.A2ChequeBook[i]
+                if Fix_measure(mes_A1) == 1   and Fix_measure(mes_owf_qubit) == 1:
+                    Y | self.A2ChequeBook[i]
+
+                #cheque_i = {'USN': db_id, 'r': r, 'Money': self.Money}
+                #self.cheque.append(cheque_i)
+
+        self.node_A.ports["portQ_A2"].tx_output(self.A2ChequeBook)
+
+
+            
+            
+    
+    # C receives qubits from A then wait and send to B 
+    def AC_rec_wait_send_B(self,chequeQList):
+        print("AC_rec_wait_send called")
+        chequeQList=chequeQList.items
+        
+        self.C_Qmemory.put(chequeQList) 
+        
+        # wait for some time before summit to bank
+        print("wait for ",self.C_delay, " ns")
+        My_waitENVtype = EventType("WAIT_EVENT", "Wait for N nanoseconds")
+        self._schedule_after(self.C_delay, My_waitENVtype) # self.delay
+        self._wait_once(ns.EventHandler(self.CpopMem),entity=self
+            ,event_type=My_waitENVtype) # can't add event_type
+        
+    
+
+    def CpopMem(self,event):
+        #print("time: ",ns.sim_time())
+        # pop out from qmem
+        sening_qList=[]
+        sening_qList=self.C_Qmemory.pop(list(np.arange(self.num_bits))) #pop all
+        print("poped from Qmem:",sening_qList)
+        self.node_C.ports["portQ_C2"].tx_output(sening_qList)
+        
+        
+        
+    # a list of qubits is verified by bank
+    def CB_Verify(self,chequeQList):
         print("CB_Verify called")
-        chequeQ=chequeQ.items
-        print(chequeQ[0].qstate.dm)
+        chequeQList=chequeQList.items
         
+        # error correction
+        for i in range(self.num_bits):
+            H | self.BChequeBook[i]
+            bob_measurement = ns.qubits.qubitapi.measure(self.BChequeBook[i]
+                        ,observable=Z)
+            #print("bob_measurement=",bob_measurement)                  
+            if Fix_measure(bob_measurement) == 1:
+                Z | chequeQList[i]
+                
+        test_var = 'n'#input("Use a fake check? (y/n)")
+        res_closeness=[]
         
+        for i in range(self.num_bits):
+            if test_var == 'n':
+                owf_bank_state = OneWayFunction('Bob',self.key_BB84_B,self.B_saveUSN[i],self.Money)
+            elif (test_var == 'y'):
+                owf_bank_state = OneWayFunction('Bob',[1,0,1,1,0,1],0,20)
+ 
+            res_closeness.append(SwapTest(owf_bank_state, chequeQList[i]))
+        
+        sum=0
+        print('Resulting array of SWAP test: ', res_closeness )
+        for i in range(len(res_closeness)):
+            if int(res_closeness[i][0])==0:
+                sum+=res_closeness[i][1]
+            else:
+                sum+=1-res_closeness[i][1]
+                
+                
+        self.chequeCloseness=sum/len(res_closeness)
+        #print(self.chequeCloseness)
+        
+        if self.chequeCloseness>self.Threshold:
+            # pass
+            print('Verified! Cheque Accepted!')
+        else:
+            # fail
+            print('FAILED! ABORTING!' )
+    
+    
     
     
     # base function ========================================================
     
     def __init__(self, num_bits=8,fibre_len_AC=10**-6
-                 ,fibre_len_AB=10**-6,fibre_len_BC=10**-6): 
+                 ,fibre_len_AB=10**-6,fibre_len_BC=10**-6
+                ,Money=1234,depolar_rate=0,time_independent=False, 
+                 Threshold=0.95,C_delay=0): 
         super().__init__()
-        self.node_A = Node("A",ID=0,port_names=["portQ_A1","portC_A1","portC_A2","portQ_A2"])
-        self.node_B = Node("B",ID=1,port_names=["portQ_B1","portC_B1","portC_B2","portQ_B2"])
+        self.node_A = Node("A",ID=0,port_names=["portQ_A1","portC_A1","portC_A2","portQ_A2","portC_A3"])
+        self.node_B = Node("B",ID=1,port_names=["portQ_B1","portC_B1","portC_B2","portQ_B2","portC_B3"])
         self.node_C = Node("C",ID=2,port_names=["portQ_C1","portQ_C2"])
+        
+        self.C_Qmemory = QuantumMemory("C_QMemory", num_bits,ID=0, 
+            memory_noise_models=DepolarNoiseModel(depolar_rate=depolar_rate,
+            time_independent=time_independent))  #normally time_independent is False unless using probability
+        self.C_delay = C_delay
         
         self.MyQfiber_AC = None
         self.MyQfiber_AB = None
@@ -197,15 +343,21 @@ class QuantumCheque(Protocol):
         self.fiberLenth_BC = fibre_len_BC
         
         self.num_bits = num_bits
-        self.AChequeBook = [] # qubits of self.num_bits-1
-        self.BChequeBook = [] # one qubit
-        self.saveChequeBook = None
+        self.A1ChequeBook = [] 
+        self.A2ChequeBook = [] 
+        self.BChequeBook = [] 
 
-        self.A_saveRSB=[] #random serial bits stored in A
-        self.B_saveRSB=[] #random serial bits stored in B
+        self.A_saveUSN = [] #random serial bits stored in A
+        self.B_saveUSN = [] #random serial bits stored in B
         
         self.key_BB84_A = []
         self.key_BB84_B = []
+        
+        self.cheque = []
+        
+        self.Money = Money
+        self.Threshold = Threshold
+        self.chequeCloseness = 0
         
         self.start()
         
@@ -226,24 +378,32 @@ class QuantumCheque(Protocol):
         self.MyCfiber_AB=DirectConnection("CFibreConn_AB",
                 ClassicalFibre("CFibre_A->B", length=self.fiberLenth_AB),
                 ClassicalFibre("CFibre_B->A", length=self.fiberLenth_AB))
+        self.MyCfiber_AB2=DirectConnection("CFibreConn_AB2",
+                ClassicalFibre("CFibre_A->B", length=self.fiberLenth_AB),
+                ClassicalFibre("CFibre_B->A", length=self.fiberLenth_AB))
         self.MyCfiber_BA=DirectConnection("CFibreConn_BA",
                 ClassicalFibre("CFibre_A->B", length=self.fiberLenth_AB),
                 ClassicalFibre("CFibre_B->A", length=self.fiberLenth_AB))
         
         
         self.MyQfiber_AC=QuantumFibre("MyQFibre_AC", length=self.fiberLenth_AC, 
-            loss_model="default")
+            loss_model=None)#"default"
         self.MyQfiber_AB=QuantumFibre("MyQFibre_AB", length=self.fiberLenth_AB, 
-            loss_model="default")
+            loss_model=None)
         self.MyQfiber_BC=QuantumFibre("MyQFibre_BC", length=self.fiberLenth_BC, 
-            loss_model="default")
+            loss_model=None)
         
-        self.node_B.connect_to(self.node_A, self.MyQfiber_AB,
-            local_port_name="portQ_B1", remote_port_name="portQ_A1")
+
         self.node_A.connect_to(self.node_B, self.MyCfiber_AB,
             local_port_name="portC_A1", remote_port_name="portC_B1")
+        self.node_B.connect_to(self.node_A, self.MyQfiber_AB,
+            local_port_name="portQ_B1", remote_port_name="portQ_A1")
+        self.node_A.connect_to(self.node_B, self.MyCfiber_AB2,
+            local_port_name="portC_A2", remote_port_name="portC_B2")
         self.node_B.connect_to(self.node_A, self.MyCfiber_BA,
-            local_port_name="portC_B2", remote_port_name="portC_A2")
+            local_port_name="portC_B3", remote_port_name="portC_A3")
+        
+        
         self.node_A.connect_to(self.node_C, self.MyQfiber_AC,
             local_port_name="portQ_A2", remote_port_name="portQ_C1")
         self.node_C.connect_to(self.node_B, self.MyQfiber_BC,
@@ -252,22 +412,29 @@ class QuantumCheque(Protocol):
         
         
         # set callback functions===================================================
-        self.node_A.ports["portQ_A1"].bind_input_handler(self.A_RSG_send_B)
-        self.node_B.ports["portC_B1"].bind_input_handler(self.BA_BB84_keygen)
-        self.node_A.ports["portC_A2"].bind_input_handler(self.A_owf_send_C)
-        self.node_C.ports["portQ_C1"].bind_input_handler(self.CB_Verify)
+ 
+
+        self.node_B.ports["portC_B1"].bind_input_handler(self.B_send_GHZ)
+        self.node_A.ports["portQ_A1"].bind_input_handler(self.A_req_USN)
+        self.node_B.ports["portC_B2"].bind_input_handler(self.B_USN_send_A)
+        self.node_A.ports["portC_A3"].bind_input_handler(self.A_owf_send_C) 
+        self.node_C.ports["portQ_C1"].bind_input_handler(self.AC_rec_wait_send_B)
+        self.node_B.ports["portQ_B2"].bind_input_handler(self.CB_Verify)
         
-        
-        # Start by bank
-        self.B_send_checkbook()
-        
+        # Start by BB84
+        self.BA_BB84_keygen()
         
         
 
 
 
 ns.sim_reset()
-qc=QuantumCheque(10)
+qc=QuantumCheque(num_bits=10,C_delay=0,Money=10)#10**9
+
 ns.sim_run()
+print(qc.Money)
+print(qc.chequeCloseness)
+
+
 
 
