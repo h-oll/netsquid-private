f967e1d86e7e4acc95567f1677e9791f2788b864
LiaoChinTe <liao.chinte@gmail.com>
2020-02-07 11:21:15 +0100

    refine and plot

--- E91/E91_QProcessor.py
+++ E91/E91_QProcessor.py
@@ -71,7 +71,7 @@ def Compare_basis(loc_basis_list,rec_basis_list,loc_meas):
 
 '''
 Ramdomly measure a list of qubits and keep the basis used. If there's a qubit missing, the list append -1.
-P.S. The qubit index goes 2 by 2 in this function. Can't work if the first one is lost.
+P.S. The qubit index goes 2 by 2 in this function. Doesn't work if the first qubit of qList is lost.
 
 input:
     Number of qubits(int)
@@ -82,7 +82,7 @@ output:
 
 '''
 def Random_ZX_measure(num_bits,qlist):
-    num_start=int(qlist[0].name[3:-len('-')-1])# get value after qubit name "QS#<i>-n"
+    num_start=int(qlist[0].name[3:-len('-')-1])# get value after qubit name "QS#<num>-x"
     basisList = []*num_bits  # set boundary
     loc_res_measure=[]*num_bits  # set boundary
     ind=0
@@ -143,38 +143,12 @@ class QG_A_qGen(QuantumProgram):
         
 
 
-
-# not used
-class QG_B_measure(QuantumProgram):
-    
-    def __init__(self,basisList,num_bits):
-        self.basisList=basisList
-        self.num_bits=num_bits
-        super().__init__()
-        
-    # ==================issue with interface ======================================
-        
-    def program(self):   # apply measurement on qubits in quantum memory. 
-        qList=self.get_qubit_indices(self.num_bits)
-        
-        if len(self.basisList) != self.num_bits:
-            print("ERROR, len(basisList) != self.num_bits ")
-        else:
-            for i in range(len(self.basisList)):
-                if self.basisList[i] == 0:                                # standard basis
-                    self.apply(INSTR_MEASURE, qList[i], output_key="meas",physical=True)
-                else :                                                    # Hadamard basis
-                    self.apply(INSTR_MEASURE_X, qList[i], output_key="meas",physical=True)
-                    
-        yield self.run(parallel=False)
-
-
-# In[7]:
+# In[27]:
 
 
 class E91_A(LocalProtocol):
     
-    def __init__(self,node,processor,num_bits):
+    def __init__(self,node,processor,num_bits,portNameQ1="portQA_1",portNameC1="portCA_1",portNameC2="portCA_2"):
         self.node=node
         self.processor=processor
         self.EPRList=None
@@ -182,8 +156,10 @@ class E91_A(LocalProtocol):
         self.Basis=Random_basis_gen(self.num_bits)
         self.loc_mesRes=[]
         self.key=None
+        self.portNameQ1=portNameQ1
+        self.portNameC2=portNameC2
         
-        self.node.ports["portCA_1"].bind_input_handler(self.A_compare_basis)
+        self.node.ports[portNameC1].bind_input_handler(self.A_compare_basis)
 
 
     def A_prepare_qubits(self):
@@ -201,7 +177,7 @@ class E91_A(LocalProtocol):
     def A_sendEPR(self):
         self.processor.sendfromMem(senderNode=self.node
             ,inx=list(range(1,2*self.num_bits+1,2))   # get even index in qList
-            ,senderPortName=self.node.ports["portQA"].name)
+            ,senderPortName=self.node.ports[self.portNameQ1].name)
         
         
     def A_measure(self):
@@ -216,10 +192,11 @@ class E91_A(LocalProtocol):
             else:
                 self.loc_mesRes.append(ns.qubits.qubitapi
                     .measure(qList[i],observable=X)[0]) #measure in Hadamard basis
-                
+        '''
+        #debug
         #print("\nA Basis:",self.Basis)
         #print("A mesRes:",self.loc_mesRes)
-        
+        '''
         
     def A_compare_basis(self,basis_B):
         # A measure
@@ -229,20 +206,20 @@ class E91_A(LocalProtocol):
         if len(self.loc_mesRes)>0:
             self.loc_mesRes=Compare_basis(self.Basis,basis_B.items,self.loc_mesRes)
             self.key=''.join(map(str, self.loc_mesRes))
-            print("key of A: ",self.key)
-            self.node.ports["portCA_2"].tx_output(self.Basis)
+            #print("key of A: ",self.key)              # A final result
+            self.node.ports[self.portNameC2].tx_output(self.Basis)
         else:
             print("Error!! A no loc_mesRes")
         
         
 
 
-# In[8]:
+# In[28]:
 
 
 class E91_B(LocalProtocol):
     
-    def __init__(self,node,processor,num_bits):
+    def __init__(self,node,processor,num_bits,portNameQ1="portQB_1",portNameC1="portCB_1",portNameC2="portCB_2"):
         self.node=node
         self.processor=processor
         self.qList=None
@@ -250,9 +227,10 @@ class E91_B(LocalProtocol):
         self.Basis=Random_basis_gen(self.num_bits)
         self.loc_mesRes=[-1]*self.num_bits   # init value assume that all qubits are lost
         self.key=None
-
-        self.node.ports["portQB"].bind_input_handler(self.B_measure)
-        self.node.ports["portCB_2"].bind_input_handler(self.B_compare_basis)
+        self.portNameC1=portNameC1
+        
+        self.node.ports[portNameQ1].bind_input_handler(self.B_measure)
+        self.node.ports[portNameC2].bind_input_handler(self.B_compare_basis)
     
                
     def B_measure(self,message):
@@ -265,101 +243,172 @@ class E91_B(LocalProtocol):
         #pop qubits from memory then measure
         mqList=[]
         mqList=self.processor.pop(positions=list(range(self.num_bits)))
-        #print("B popped qubits:\n",mqList)
         
         self.Basis,self.loc_mesRes=Random_ZX_measure(self.num_bits,mqList)
+        '''
+        #debug
         #print("\nB Basis:",self.Basis)
         #print("B mesRes:",self.loc_mesRes)
-        
+        '''
         self.B_send_basis()
         
         
     def B_send_basis(self):
-        self.node.ports["portCB_1"].tx_output(self.Basis)
+        self.node.ports[self.portNameC1].tx_output(self.Basis)
         
         
     def B_compare_basis(self,basis_A):
         if len(self.loc_mesRes)>1: # exclude empty case
             self.loc_mesRes=Compare_basis(self.Basis,basis_A.items,self.loc_mesRes)
             self.key=''.join(map(str, self.loc_mesRes))
-            print("key of B: ",self.key)
+            #print("key of B: ",self.key)   #B final result
         else:
             print("Error no loc_mesRes!")
         
         
 
 
-# In[11]:
+# In[29]:
 
 
-# environment setting
+# implementation
 
-ns.sim_reset()
-
-Alice_processor=sendableQProcessor("processor_A", num_positions=100,
-            mem_noise_models=[DepolarNoiseModel(0)] * 100, phys_instructions=[
-            PhysicalInstruction(INSTR_INIT, duration=1, parallel=True),
-            PhysicalInstruction(INSTR_X, duration=1, q_noise_model=None),
-            PhysicalInstruction(INSTR_Z, duration=1, q_noise_model=None),
-            PhysicalInstruction(INSTR_H, duration=1, q_noise_model=None),
-            PhysicalInstruction(INSTR_CNOT,q_noise_model=None, duration=1),
-            PhysicalInstruction(INSTR_MEASURE, duration=1, parallel=True)],
-            topologies=[None, None, None, None, None, None])
-
-
-Bob_processor=sendableQProcessor("processor_B", num_positions=100,
-            mem_noise_models=[DepolarNoiseModel(0)] * 100, phys_instructions=[
-            PhysicalInstruction(INSTR_INIT, duration=1, parallel=True),
-            PhysicalInstruction(INSTR_X, duration=1, q_noise_model=None),
-            PhysicalInstruction(INSTR_Z, duration=1, q_noise_model=None),
-            PhysicalInstruction(INSTR_H, duration=1, q_noise_model=None),
-            PhysicalInstruction(INSTR_CNOT,q_noise_model=None, duration=1),
-            PhysicalInstruction(INSTR_MEASURE, duration=1, parallel=True)],
-            topologies=[None, None, None, None, None, None])
-
-
-
-node_A = Node("A",ID=0,port_names=["portQA","portCA_1","portCA_2"])
-node_B = Node("B",ID=1,port_names=["portQB","portCB_1","portCB_2"])
-
-
-Alice=E91_A(node=node_A,processor=Alice_processor,num_bits=20)
-Bob=E91_B(node=node_B,processor=Bob_processor,num_bits=20)
-
-
-# connection
-MyQfiber=QuantumFibre("QFibre_A->B", length=10**-6,quantum_loss_model=None)
-
-
-MyCfiber=DirectConnection("CFibreConn_B->A",
-    ClassicalFibre("CFibre_B->A", length=10**-6))
-
-
-MyCfiber2=DirectConnection("CFibreConn_A->B",
-    ClassicalFibre("CFibre_A->B", length=10**-6))
-
-
-node_A.connect_to(node_B, MyQfiber,
-    local_port_name =node_A.ports["portQA"].name,
-    remote_port_name=node_B.ports["portQB"].name)
-
-
-node_B.connect_to(node_A, MyCfiber,
-            local_port_name="portCB_1", remote_port_name="portCA_1")
+def run_E91_sim(runtimes=1,num_bits=20,fibre_len=0,noise_model=None):
+    
+    MyE91List_A=[]  # local protocol list A
+    MyE91List_B=[]  # local protocol list B
+    
+    for i in range(runtimes): 
         
-node_A.connect_to(node_B, MyCfiber2,
-            local_port_name="portCA_2", remote_port_name="portCB_2")
+        ns.sim_reset()
+        
+        # Hardware configuration
+        Alice_processor=sendableQProcessor("processor_A", num_positions=100,
+                    mem_noise_models=[DepolarNoiseModel(0)] * 100, phys_instructions=[
+                    PhysicalInstruction(INSTR_INIT, duration=1, parallel=True),
+                    PhysicalInstruction(INSTR_X, duration=1, q_noise_model=noise_model),
+                    PhysicalInstruction(INSTR_Z, duration=1, q_noise_model=noise_model),
+                    PhysicalInstruction(INSTR_H, duration=1, q_noise_model=noise_model),
+                    PhysicalInstruction(INSTR_CNOT,q_noise_model=noise_model, duration=1),
+                    PhysicalInstruction(INSTR_MEASURE, duration=1, parallel=True)],
+                    topologies=[None, None, None, None, None, None])
 
 
+        Bob_processor=sendableQProcessor("processor_B", num_positions=100,
+                    mem_noise_models=[DepolarNoiseModel(0)] * 100, phys_instructions=[
+                    PhysicalInstruction(INSTR_INIT, duration=1, parallel=True),
+                    PhysicalInstruction(INSTR_X, duration=1, q_noise_model=noise_model),
+                    PhysicalInstruction(INSTR_Z, duration=1, q_noise_model=noise_model),
+                    PhysicalInstruction(INSTR_H, duration=1, q_noise_model=noise_model),
+                    PhysicalInstruction(INSTR_CNOT,q_noise_model=noise_model, duration=1),
+                    PhysicalInstruction(INSTR_MEASURE, duration=1, parallel=True)],
+                    topologies=[None, None, None, None, None, None])
 
-# begin to run protocols
-for i in Alice.A_prepare_qubits():
-    pass
+
+        node_A = Node("A",ID=0,port_names=["portQA_1","portCA_1","portCA_2"])
+        node_B = Node("B",ID=1,port_names=["portQB_1","portCB_1","portCB_2"])
 
 
-#ns.logger.setLevel(1)
-ns.sim_run()
+        # connection
+        MyQfiber=QuantumFibre("QFibre_A->B", length=fibre_len,quantum_loss_model=None) 
+        # default value:
+        # p_loss_init=0.2, p_loss_length=0.25,depolar_rate=0, c=200000, models=None
 
+
+        MyCfiber=DirectConnection("CFibreConn_B->A",
+            ClassicalFibre("CFibre_B->A", length=fibre_len))
+
+
+        MyCfiber2=DirectConnection("CFibreConn_A->B",
+            ClassicalFibre("CFibre_A->B", length=fibre_len))
+
+
+        node_A.connect_to(node_B, MyQfiber,
+            local_port_name =node_A.ports["portQA_1"].name,
+            remote_port_name=node_B.ports["portQB_1"].name)
+
+
+        node_B.connect_to(node_A, MyCfiber,
+                    local_port_name="portCB_1", remote_port_name="portCA_1")
+
+        node_A.connect_to(node_B, MyCfiber2,
+                    local_port_name="portCA_2", remote_port_name="portCB_2")
+        
+        
+        
+        Alice=E91_A(node=node_A,processor=Alice_processor,num_bits=num_bits
+            ,portNameQ1="portQA_1",portNameC1="portCA_1",portNameC2="portCA_2")
+        Bob=E91_B(node=node_B,processor=Bob_processor,num_bits=num_bits
+            ,portNameQ1="portQB_1",portNameC1="portCB_1",portNameC2="portCB_2")
+        
+        
+        for i in Alice.A_prepare_qubits():
+            pass
+        
+        ns.sim_run()
+        
+        MyE91List_A.append(Alice.key)
+        MyE91List_B.append(Bob.key)
+        
+    return MyE91List_A, MyE91List_B
+
+
+# In[30]:
+
+
+# plot function
+import matplotlib.pyplot as plt
+
+def E91_plot():
+    y_axis=[]
+    x_axis=[]
+    run_times=10
+    num_bits=50
+    min_dis=1000
+    max_dis=15000
+
+    # first curve
+    for i in range(min_dis,max_dis,1000):
+        key_sum=0.0
+        x_axis.append(1.*i/1000)
+        key_list_A,key_list_B=run_E91_sim(run_times,num_bits,1.*i/1000
+            ,noise_model=None) #feed runtimes, numberof bits and distance, use default loss model
+        for keyA,keyB in zip(key_list_A,key_list_B):
+            if keyA==keyB:  #else error happend, drop key, count 0 length
+                key_sum=key_sum+len(keyA)
+        y_axis.append(key_sum/run_times/num_bits)
+        
+    plt.plot(x_axis, y_axis, 'go-',label='without noise model')
+    
+    
+    y_axis.clear() 
+    x_axis.clear()
+    
+    # second curve
+    for i in range(min_dis,max_dis,1000):
+        key_sum=0.0
+        x_axis.append(1.*i/1000)
+        key_list_A,key_list_B=run_E91_sim(run_times,num_bits,1.*i/1000
+            ,noise_model=DepolarNoiseModel(depolar_rate=500)) #feed runtimes, numberof bits and distance, use default loss model
+        for keyA,keyB in zip(key_list_A,key_list_B):
+            if keyA==keyB:  #else error happend, drop key, count 0 length
+                key_sum=key_sum+len(keyA)
+        y_axis.append(key_sum/run_times/num_bits)
+        
+    plt.plot(x_axis, y_axis, 'bo-',label='DepolarNoiseModel depolar rate:500')
+      
+        
+    plt.ylabel('average key length/original qubits length')
+    plt.xlabel('fibre lenth (km)')
+    
+    
+    plt.legend()
+    plt.savefig('plot.png')
+    plt.show()
+
+    
+
+E91_plot()
 
 
 # In[ ]:
