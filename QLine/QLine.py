5d95285e3125b5e3bc8f1cddccbf8d30cc5f2b13
LiaoChinTe <liao.chinte@gmail.com>
2020-03-12 15:46:47 +0100

    refine

--- QLine/QLine.py
+++ QLine/QLine.py
@@ -47,18 +47,8 @@ from netsquid.components.models.qerrormodels import *
 from random import randint
 
 
-# In[12]:
+# In[2]:
 
-
-
-def CheckConnection(NodeList):
-    for node in NodeList:
-        for otherNode in NodeList:
-            if node.get_conn_port(otherNode.ID,label='Q') :
-                print(node.get_conn_port(otherNode.ID,label='Q'))
-            if node.get_conn_port(otherNode.ID,label='C'):
-                print(node.get_conn_port(otherNode.ID,label='C'))
-    print("==================")
 
 # get output value from a Qprogram
 def getPGoutput(Pg,key):
@@ -136,7 +126,7 @@ class QG_D(QuantumProgram):
         yield self.run(parallel=False)
 
 
-# In[13]:
+# In[5]:
 
 
 class fQLine(Protocol):
@@ -157,11 +147,11 @@ class fQLine(Protocol):
         self.c=randint(0,1)
         self.s=randint(0,1)
         
-        self.keyAB=None
-        self.keyBA=None
+        self.keyI=None
+        self.keyT=None
         
-        self.TimeF=0.0
-        self.TimeD=0.0
+        self.TimeF=0.0   #time recorded when started
+        self.TimeD=0.0   #time cost for this bit to form
         
         # if not forwarded, connect them with Q,C fibres
         if self.nodeList[self.initNodeID].get_conn_port(
@@ -236,36 +226,35 @@ class fQLine(Protocol):
             self.targetNodeID-1,label='Q').bind_input_handler(
             self.T_Rec_HX)
         
-        self.O_Qubit_prepare()
+        # record time to start
+        self.TimeF=ns.util.simtools.sim_time(magnitude=ns.NANOSECOND)
+        
+        self.O_QubitGen()
         
 
 # O =========================================== 
-
-    def O_Qubit_prepare(self):
+    def O_Fail(self):
+        print("O_Fail")
+    
+    # must
+    def O_QubitGen(self):
         #print("O_Qubit_prepare")
         myQG_O=QG_O()
         self.processorList[0].execute_program(
             myQG_O,qubit_mapping=[0])
-        self.processorList[0].set_program_done_callback(
-            self.O_QubitSend,once=True)
         self.processorList[0].set_program_fail_callback(
-            self.O_QG_Failed,once=True)
-        
+            self.O_Fail,once=True)
         
         if self.initNodeID==0: # if I==O !!!
             self.processorList[0].set_program_done_callback(
                 self.I_QG_HX,once=True)
-        else:
+        else:     # I!=O
             self.processorList[0].set_program_done_callback(
-                self.O_QubitSend,once=True)
+                self.O_Send_I,once=True)
         
     
-    def O_QG_Failed(self):
-        print("QG_Failed")
-    
-    
-    # when O != I
-    def O_QubitSend(self):
+    # O != I
+    def O_Send_I(self):
         #print("O_QubitSend")
         # I callback
         self.nodeList[self.initNodeID].get_conn_port(
@@ -277,11 +266,17 @@ class fQLine(Protocol):
             ,receiveNode=self.nodeList[1]) 
 
 # I  ==================================================
+    def I_Fail(self):
+        self.qubitLoss=True
+        print("I_Fail")   
+
+    # O != I
     def I_rec(self,qubit):
         #print("I_rec")
         self.processorList[self.initNodeID].put(qubits=qubit.items)
         self.I_QG_HX()
         
+    # must
     def I_QG_HX(self):
         #print("I_QG_HX")
         myQG_I=QG_I(self.r,self.b)
@@ -290,27 +285,29 @@ class fQLine(Protocol):
         self.processorList[self.initNodeID].set_program_done_callback(
             self.I_QubitSend,once=True)
         self.processorList[self.initNodeID].set_program_fail_callback(
-            self.I_QG_Failed,once=True)
+            self.I_Fail,once=True)
         
-    def I_QG_Failed(self):
-        print("QG_I_Failed")     
-        
+
+    # must
     def I_QubitSend(self):
         #print("I_QubitSend")
+        
         # I callback
         self.nodeList[self.initNodeID].get_conn_port(
             self.initNodeID+1, label='C').bind_input_handler(self.I_Compare)
-
+        
+        # send qubit
         self.processorList[self.initNodeID].sendfromMem(
             senderNode=self.nodeList[self.initNodeID],inx=[0]
-            ,receiveNode=self.nodeList[self.initNodeID+1]) # get even index in qList
+            ,receiveNode=self.nodeList[self.initNodeID+1])
 
 
 # T  ==================================================
-    def T_PG_Fail(self):
+    def T_Fail(self):
         self.qubitLoss=True
-        print("PG_targetFail!")
+        print("T_Fail")
         
+    # must
     def T_Rec_HX(self,qubit):
         #print("T_Rec_HX: ",qubit.items)
         self.processorList[self.targetNodeID].put(qubits=qubit.items)
@@ -320,16 +317,17 @@ class fQLine(Protocol):
         self.processorList[self.targetNodeID].execute_program(
             self.myQG_T,qubit_mapping=[0])
         self.processorList[self.targetNodeID].set_program_fail_callback(
-            self.T_PG_Fail,once=True)
+            self.T_Fail,once=True)
         
         # if T==D !!!
         if self.targetNodeID==len(self.nodeList)-1: 
             # send s R back to T then I
             #len(self.nodeList)-1==self.targetNodeID
             self.processorList[self.targetNodeID].set_program_done_callback(
-                self.D_meas,once=True)
-        
-        else: # T!=D !!! send forward
+                self.D_QG_measure,once=True)
+            
+        # T!=D !!! send forward
+        else: 
             # set T callback for R
             self.nodeList[self.targetNodeID].get_conn_port(
                 self.targetNodeID+1, label='C').bind_input_handler(self.T_recR)
@@ -340,9 +338,6 @@ class fQLine(Protocol):
             #send to D
             self.processorList[self.targetNodeID].set_program_done_callback(
                 self.T_Send_D,once=True)
-            # get even index in qList
-
-
         
     # T!=D
     def T_Send_D(self):
@@ -350,86 +345,83 @@ class fQLine(Protocol):
         self.processorList[self.targetNodeID].sendfromMem(
             senderNode=self.nodeList[self.targetNodeID],inx=[0]
             ,receiveNode=self.nodeList[self.targetNodeID+1])
-        #self.nodeList[self.targetNodeID].get_conn_port(
-        #    self.targetNodeID-1, label='C').bind_input_handler(self.T_Compare)
 
 #  D ==================================================
-    # must
-    def D_meas(self):
-        #print("D_meas")
-        self.myQG_D=QG_D()
-        # lastnode.ID==len(self.nodeList)-1
-        #print(self.nodeList[-1])
-        #print(self.nodeList[len(self.nodeList)-1])
-        self.processorList[-1].execute_program(
-            self.myQG_D,qubit_mapping=[0])
-        self.processorList[-1].set_program_fail_callback(
-            self.D_Fail,once=True)
-        self.processorList[-1].set_program_done_callback(
-            self.D_sendback,once=True)
-
     def D_Fail(self):
+        self.qubitLoss=True
         print("D_Fail")
 
     # if T!=D
     def D_rec(self,qubit):
         #print("D_rec")
         self.processorList[-1].put(qubits=qubit.items)
-        self.D_meas()
-
+        self.D_QG_HX()
+        
+    # must
+    def D_QG_measure(self):
+        # print("D_QG_measure")
+        self.myQG_D=QG_D()
+        self.processorList[-1].execute_program(
+            self.myQG_D,qubit_mapping=[0])
+        self.processorList[-1].set_program_fail_callback(
+            self.D_Fail,once=True)
+        self.processorList[-1].set_program_done_callback(
+            self.D_sendback,once=True)
+    
     # must
     def D_sendback(self):
         #print("D_sendback")
         self.R=getPGoutput(self.myQG_D,'R')
         self.nodeList[len(self.nodeList)-1].get_conn_port(
             len(self.nodeList)-2,label='C').tx_output([self.s,self.R])
-        
+
+
+#Following are phases that we use classical fibres only.
+
 # C1 ==================================================
 
+    # receive R then send back
     def T_recR(self,R):
-        # receive R then send back
         self.nodeList[self.targetNodeID].get_conn_port(
             self.targetNodeID-1,label='C').tx_output([self.s,R.items[0]])
-        
-    
+
+
 # C2 ==================================================    
     def I_Compare(self,alist): # receives [self.s,self.R]
         #print("InitNodeCompare")
         if alist.items[0]==self.r: # if s == r
-            self.keyAB=self.b
+            self.keyI=self.b
         self.I_Response()
 
     def I_Response(self):
         #print("InitNodeResponse")
+        
         # callback T compare
         self.nodeList[self.targetNodeID].get_conn_port(
             self.targetNodeID-1, label='C').bind_input_handler(self.T_Compare)
-
         self.nodeList[self.initNodeID].get_conn_port(
             self.initNodeID+1,label='C').tx_output(self.r)
-      
-    
-# C3 =====================================================================
 
+# C3 =====================================================================
     def T_Compare(self,r):
         #print("TargetNodeCompare")
+        
         self.qubitLoss=False
         if self.s==r.items[0]:
-            self.keyBA=self.R ^ self.c
+            self.keyT=self.R ^ self.c
         self.TimeD=ns.util.simtools.sim_time(magnitude=ns.NANOSECOND)-self.TimeF 
         #in nanoseconds    
 
 
-# In[16]:
+# In[6]:
 
 
-
-def run_QLine_sim(times=1,fibre_len=10**-3,noise_model=None): # fibre 1 m long
+def run_QLine_sim(times=1,fibre_len=10**-3,noise_model=None):
     
-    keyListAB=[]
-    keyListBA=[]
+    keyListI=[]
+    keyListT=[]
     
-    # A B Hardware configuration
+    # Hardware configuration
     processorA=sendableQProcessor("processor_A", num_positions=1,
                 mem_noise_models=None, phys_instructions=[
                 PhysicalInstruction(INSTR_INIT, duration=1, parallel=True),
@@ -468,26 +460,20 @@ def run_QLine_sim(times=1,fibre_len=10**-3,noise_model=None): # fibre 1 m long
         ns.sim_reset()
         myQLine=fQLine(nodeList=[node_A,node_B,node_C,node_D]
             ,processorList=[processorA,processorB,processorC,processorD]
-            ,initNodeID=1,targetNodeID=3)
-        #myQLine.ForwardSetting(0,3)
+            ,initNodeID=0,targetNodeID=3)
         myQLine.start()
         ns.sim_run()
-        if myQLine.keyAB != None :
-            keyListAB.append(myQLine.keyAB)
-            keyListBA.append(myQLine.keyBA)
-    print(keyListAB)
-    print(keyListBA)
+        
+        if myQLine.keyI != None :
+            keyListI.append(myQLine.keyI)
+            keyListT.append(myQLine.keyT)
+    print(keyListI)
+    print(keyListT)
 
 
 #test
 #ns.logger.setLevel(1)
 run_QLine_sim(times=10,fibre_len=10**-3,noise_model=None) 
-
-
-# In[ ]:
-
-
-
 
 
 # In[ ]:
